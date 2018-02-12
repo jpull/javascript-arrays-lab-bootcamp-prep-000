c13f07d9b1fddbd8c8d0fd93bb3515663f193073
Jerry P <jpull@jpull.com>
2018-02-12 19:39:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,6 @@ function prependKitten(name) {
 function removeLastKitten() {
   return kittens.slice(0,kittens.length-1)
 }
+function removeLastKitten() {
+  return kittens.slice(1,kittens.length-1)
+}
